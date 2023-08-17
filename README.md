5e04a6c108b525bd6fb2e7e60367a3a842164564
DipItlogiko <dipitlogiko@gmail.com>
2023-08-17 11:21:48 +0600

    Hide account or logout links when not authenticated

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # here i have updated some functionalities of my social network project such as:
 
 * Hide like or dislike button for own posts.
+* Hide account or logout links when not authenticated.
  
--- resources/views/includes/headers.blade.php
+++ resources/views/includes/headers.blade.php
@@ -15,10 +15,10 @@
     <!-- Collect the nav links, forms, and other content for toggling -->
     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
       <ul class="nav navbar-nav navbar-right">
-        
+      @auth  
         <li><a href="{{ route('account') }}">Account</a></li>
         <li><a href="{{ route('logout') }}">Logout</a></li>
-      
+      @endauth
       </ul>       
        
     </div><!-- /.navbar-collapse -->
